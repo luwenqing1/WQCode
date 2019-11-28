9d289424419d72c9ccae8d2965f5d2bf8edf863c
luwenqing <luwenqing@szboanda.net>
2019-11-28 15:41:34 +0800

    添加组件

---
+++ NewTest1/Classes/WQSearchView.m
@@ -0,0 +1,22 @@
+//
+//  WQSearchView.m
+//  serchControler
+//
+//  Created by lwq on 2019/11/28.
+//  Copyright © 2019 luwenqing. All rights reserved.
+//
+
+#import "WQSearchView.h"
+
+@implementation WQSearchView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        //加点注释
+    }
+    return self;
+}
+
+@end
